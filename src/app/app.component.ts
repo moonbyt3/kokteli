3efaf73c5e192e3d69cc240299e135e6005ac5f1
moonbyt3 <moonbyt3@gmail.com>
2018-02-21 15:49:52 +0100

    Refractored code, added few cocktails

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -199,6 +199,166 @@ const mockCoctailDatabase = {
     },
     {
       "idDrink": "11",
+      "drink": "Johnnie Black Sweet & Sour",
+      "video": null,
+      "category": "Klasični koktel",
+      "alcoholic": true,
+      "glass": "Ohlađena čaša za koktel",
+      "instructions": "U sejker dodajte sastojke i mesajte dok se svi sastojci ne pomesaju. Zatim dodajte dosta leda i mesajte dok sejker ne postane hladan. Dekorisite sa kriskama limuna.",
+      "drinkThumb": "assets/img/name.jpg",
+      "ingredients": [
+        "Johnnie Black - 0.03ml",
+        "Triple Sec Narandza - 0.015ml",
+        "Sok od limuna - 0.02ml",
+        "Belance - 1 jaje",
+        "Sirup od šećera - 0.015ml"
+      ],
+      "dateModified": "2018-02-12 16:21:31"
+    },
+    {
+      "idDrink": "12",
+      "drink": "Moscow Mule",
+      "video": null,
+      "category": "Klasicni kokteli",
+      "alcoholic": true,
+      "glass": "Ohlađena čaša za koktel",
+      "instructions": "Sipajte vodku u casu, dodati svezu limetu iseckanu. Dodajte sirup od secera. Sve to dobro iznjecite zatim dodajte sirup od djumbira i napuniti casu punu leda i dodati sprajt do vrha case. Dekorisati sa limetom i nekoliko listova nane",
+      "drinkThumb": "assets/img/name.jpg",
+      "ingredients": [
+        "Vodka - 0.06ml",
+        "Limeta - 1/2",
+        "Sirup od šećera - 0.015ml",
+        "Sirup od đumbira - 0.015ml",
+        "Sprajt",
+        "Nana - nekoliko listova"
+      ],
+      "dateModified": "2018-02-12 16:21:31"
+    },
+    {
+      "idDrink": "13",
+      "drink": "Hard Lemonade",
+      "video": null,
+      "category": "Klasicni kokteli",
+      "alcoholic": true,
+      "glass": "Ohlađena čaša za koktel",
+      "instructions": "Sipajte sve sastojke u sejker i dodajte led. Mesajte dok sejker ne postane hladan. Koktel je spreman za serviranje, dekorisite ga sa limunom ili limetom.",
+      "drinkThumb": "assets/img/name.jpg",
+      "ingredients": [
+        "Vodka - 0.06ml",
+        "Limunov sok - 0.01ml",
+        "Sirup od secera - 0.03ml",
+        "Mineralna voda"
+      ],
+      "dateModified": "2018-02-12 16:21:31"
+    },
+    {
+      "idDrink": "14",
+      "drink": "Rose Cosmo",
+      "video": null,
+      "category": "Klasicni kokteli",
+      "alcoholic": true,
+      "glass": "Ohlađena čaša za martini",
+      "instructions": "",
+      "drinkThumb": "assets/img/name.jpg",
+      "ingredients": [
+        "Vodka Smirnoff - 0.03ml",
+        "Limunov sok - 0.01ml",
+        "Triple sec - 0.02ml",
+        "Sirup od ruze - 0.02ml",
+        "Sok od borovnice"
+      ],
+      "dateModified": "2018-02-12 16:21:31"
+    },
+    {
+      "idDrink": "15",
+      "drink": "Metropolitian",
+      "video": null,
+      "category": "Klasicni kokteli",
+      "alcoholic": true,
+      "glass": "Ohlađena čaša za martini",
+      "instructions": "",
+      "drinkThumb": "assets/img/name.jpg",
+      "ingredients": [
+        "Vodka - 0.03ml",
+        "Triple sec - 0.03ml",
+        "Sok od borovnice - 0.045ml",
+        "Sok od limete - 0.02ml"
+      ],
+      "dateModified": "2018-02-12 16:21:31"
+    },
+    {
+      "idDrink": "16",
+      "drink": "Caipirosca",
+      "video": null,
+      "category": "",
+      "alcoholic": true,
+      "glass": "0.2dl",
+      "instructions": "",
+      "drinkThumb": "assets/img/name.jpg",
+      "ingredients": [
+        "Vodka - 0.06ml",
+        "Limeta - 1/2",
+        "Smedji secer - 2 kasicice",
+        "Led"
+      ],
+      "dateModified": "2018-02-12 16:21:31"
+    },
+    {
+      "idDrink": "17",
+      "drink": "Fruity Carpirosca",
+      "video": null,
+      "category": "",
+      "alcoholic": true,
+      "glass": "Ohlađena čaša za koktel",
+      "instructions": "",
+      "drinkThumb": "assets/img/name.jpg",
+      "ingredients": [
+        "Vodka - 0.06ml",
+        "Smedji secer - 2 kasicice",
+        "Limeta - 1/2",
+        "Sveze voce - sitno iseckano",
+        "Led - mleveni"
+      ],
+      "dateModified": "2018-02-12 16:21:31"
+    },
+    {
+      "idDrink": "18",
+      "drink": "Pear Elder Flower Martini",
+      "video": null,
+      "category": "Klasicni kokteli",
+      "alcoholic": true,
+      "glass": "Ohlađena čaša za martini",
+      "instructions": "",
+      "drinkThumb": "assets/img/name.jpg",
+      "ingredients": [
+        "Vodka - 0.05ml",
+        "Sok od kruske - 0.065ml",
+        "Sok od limuna - 0.015ml",
+        "Sirup od zove - 0.015ml"
+      ],
+      "dateModified": "2018-02-12 16:21:31"
+    },
+    {
+      "idDrink": "19",
+      "drink": "MoMo Special",
+      "video": null,
+      "category": "",
+      "alcoholic": true,
+      "glass": "Ohlađena čaša za koktel",
+      "instructions": "",
+      "drinkThumb": "assets/img/name.jpg",
+      "ingredients": [
+        "Vodka - 0.06ml",
+        "Sok od limuna - 0.015ml",
+        "Sirup od secera - 0.015ml",
+        "Mineralna voda",
+        "Nana",
+        "Limeta"
+      ],
+      "dateModified": "2018-02-12 16:21:31"
+    },
+    {
+      "idDrink": "20",
       "drink": "name",
       "video": null,
       "category": "",
